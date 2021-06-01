5a43f8ec8d7cef9234554f3e2915bf05b35ce0a2
Roberto Candales <candales.roberto@gmail.com>
2021-06-01 15:19:20 -0300

    starting slot machine challenge

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from 'react-redux';
+import Container from './components/container/Container';
+import { Title } from './styles';
+import generateStore from './redux/store/store';
 
 function App() {
+  const store = generateStore();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Title>Slot Machine</Title>
+      <Container />
+    </Provider>
   );
 }
 
---
+++ src/components/container/styles.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const WrapperContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 100%;
+  height: 100%;
+`;
+
+export const BoxWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 5px solid red;
+  padding: 10px;
+  width: 200px;
+  height: 200px;
+  border-radius: 15px;
+  font-weight: 50px;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+`;
